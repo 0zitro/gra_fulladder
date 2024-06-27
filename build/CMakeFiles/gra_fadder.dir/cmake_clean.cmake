77d82d937c629b53d1bcd2e8a2a80f22bf02b758
Zitro <zitro@smart-teapot-418>
2024-06-27 17:32:50 +0200

    remove build/ for apparency

---
+++ build/CMakeFiles/gra_fadder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/zitro/Files/Projects/SystemC/gra24cdafadder-practice-ge92zot/bin/gra_fadder"
+  "/home/zitro/Files/Projects/SystemC/gra24cdafadder-practice-ge92zot/bin/gra_fadder.pdb"
+  "CMakeFiles/gra_fadder.dir/src/sc_main.cpp.o"
+  "CMakeFiles/gra_fadder.dir/src/sc_main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gra_fadder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/gra_fadder.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gra_fadder.
+# This may be replaced when dependencies are built.
